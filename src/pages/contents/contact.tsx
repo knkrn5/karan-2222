6209fb658d0ce736911bde4833ff9b9504fc1b56
Karan <xkaranx5@gmail.com>
2025-02-08 20:54:03 +0530

    contect

--- src/pages/contents/contact.tsx
+++ src/pages/contents/contact.tsx
@@ -27,9 +27,9 @@ function Contact() {
   };
 
   return (
-    <div className="dark:bg-dark">
+    <div className="bg-gray-100 dark:bg-dark">
       <div className="text-center mb-4">
-        <h1 className="text-4xl font-bold text-gray-900 dark:text-white mb-4">Contact Us</h1>
+        <h1 className="text-4xl font-bold text-gray-900 dark:text-white mb-4 pt-4">Contact Us</h1>
         <p className="text-lg text-gray-600 dark:text-gray-300">Have a question or want to work together?</p>
       </div>
 
