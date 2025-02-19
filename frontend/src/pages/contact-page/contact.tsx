dd97ce86f36f6da3b26261911f18aa79b98c5b5a
Karan <xkaranx5@gmail.com>
2025-02-19 09:12:34 +0530

    lg

--- frontend/src/pages/contact-page/contact.tsx
+++ frontend/src/pages/contact-page/contact.tsx
@@ -4,13 +4,13 @@ import ContactForm from './contactForm';
 
 function Contact() {
   return (
-    <div className="bg-gray-100 dark:bg-gray-800">
+    <div className=" bg-gray-100 dark:bg-gray-800">
       <div className="text-center mb-4">
         <h1 className="text-4xl font-bold text-gray-900 dark:text-white mb-4 pt-4">Contact Me</h1>
         <p className="text-lg text-gray-600 dark:text-gray-300">Have a question or want to work together?</p>
       </div>
 
-      <div className="grid grid-cols-1 p-6 lg:grid-cols-2 gap-8">
+      <div className="lg:max-w-7xl mx-auto grid grid-cols-1 p-6 lg:grid-cols-2 gap-10">
         {/* Contact Details Section */}
         <ContactDetails />
 
--- frontend/src/partials/seeData.tsx
+++ frontend/src/partials/seeData.tsx
@@ -14,8 +14,8 @@ const seeData = ({ name, email, message }: contactDataProps) => {
       <div className="flex flex-col items-center p-5">
         <h5 className="mb-1 text-xl font-medium text-gray-900 dark:text-white">{name}</h5>
         <p className="text-sm text-gray-500 dark:text-gray-400">{email}</p>
-        <p className="w-3/4 text-sm p-4 m-2 bg-white rounded-2xl  dark:bg-gray-700 dark:text-white">{message}</p>
-        <div className="flex mt-4 md:mt-6">
+        <p className="w-3/4 text-sm p-4 m-2 break-all bg-white rounded-2xl  dark:bg-gray-700 dark:text-white">{message}</p>
+        <div className="flex mt-4">
           <a
             href="#"
             className="inline-flex items-center px-4 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
