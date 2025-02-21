0d38c59b8709078103b90ad0d492bd4fde6b0a24
Karan <xkaranx5@gmail.com>
2025-02-21 23:51:48 +0530

    backend api

--- backend/src/app.js
+++ backend/src/app.js
@@ -6,7 +6,7 @@ import cookieParser from "cookie-parser";
 const app = express();
 
 app.use(cors({
-    origin: "http://localhost:3000",
+    origin: "https://karan-2222.onrender.com/",
     credentials: true
 }));
 
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3000',
+        target: 'https://karan-2222.onrender.com/',
         changeOrigin: true,
         // rewrite: (path) => path.replace(/^\/api/, ''),
       },
