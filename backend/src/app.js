fe656dd611cf744b8e1cea18171111439e16b776
Karan <xkaranx5@gmail.com>
2025-02-21 12:37:26 +0530

    validation improved

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,9 +1,7 @@
 import express from "express";
-// import dotenv from "dotenv";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 
-// dotenv.config({ path: "./backend/.env" });
 
 const app = express();
 
@@ -13,11 +11,13 @@ app.use(cors({
 }));
 
 
-//middlewares
+// Middleware setup
 app.use(express.json({limit: "16kb"}));
 app.use(express.urlencoded({limit: "16kb", extended: true}));
 app.use(express.static("public"));
 app.use(cookieParser());
+
+
 
 
 //routes imports
