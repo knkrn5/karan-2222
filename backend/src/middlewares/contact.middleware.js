e37bdd63574d7473bb97bf10fc9574d615b292bd
Karan <xkaranx5@gmail.com>
2025-02-22 10:15:56 +0530

    limit 5

--- backend/src/middlewares/contact.middleware.js
+++ backend/src/middlewares/contact.middleware.js
@@ -2,8 +2,8 @@ import rateLimit from 'express-rate-limit';
 
 const contactLimiter = rateLimit({
   windowMs: 60 * 60 * 1000, // 60 minutes
-  max: 3, // Limit each IP to 3 requests
-  message: { success: false, status: "Failed, Too many requests sent..." },
+  max: 5, // Limit each IP to 5 requests
+  message: { success: false, status: "Too many requests sent..." },
 });
 
 export { contactLimiter };
